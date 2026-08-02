a8692e6c5d4640d3882b92bb7adcc3832090d792
Joshua Goins <josh@redstrate.com>
2026-08-02 12:12:11 -0400

    Add porting reminder to CS

--- Rebound/Plugin.cs
+++ Rebound/Plugin.cs
@@ -7,6 +7,8 @@ namespace Rebound;
 
 public sealed class Plugin : IDalamudPlugin
 {
+    // TODO: Replace with BonePhysicsModule.GetForceOverrideSimulationTime() when that's available
+
     /// When true, checks if we're about ~10 FPS over 60 and enables UseOverrideSimulationTime on BonePhysicsModules.
     /// If you need to find the signature for this again, start in Client::Graphics::Scene::CharacterBase.UpdateRender.
     [Signature("38 1D ?? ?? ?? ?? F3 0F 10 3D", ScanType = ScanType.StaticAddress)]
