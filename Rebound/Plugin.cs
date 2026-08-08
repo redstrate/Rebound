25a83e1ef686a925a8ce7f93d9becd5e1b97389d
Joshua Goins <josh@redstrate.com>
2026-08-07 22:14:56 -0400

    Use CS bool property, bump to 1.7.0.2

--- Rebound/Plugin.cs
+++ Rebound/Plugin.cs
@@ -1,26 +1,19 @@
 using Dalamud.IoC;
 using Dalamud.Plugin;
 using Dalamud.Plugin.Services;
-using Dalamud.Utility.Signatures;
+using FFXIVClientStructs.FFXIV.Client.Graphics.Physics;
 
 namespace Rebound;
 
 public sealed class Plugin : IDalamudPlugin
 {
-    // TODO: Replace with BonePhysicsModule.GetForceOverrideSimulationTime() when that's available
-
-    /// When true, checks if we're about ~10 FPS over 60 and enables UseOverrideSimulationTime on BonePhysicsModules.
-    /// If you need to find the signature for this again, start in Client::Graphics::Scene::CharacterBase.UpdateRender.
-    [Signature("38 1D ?? ?? ?? ?? F3 0F 10 3D", ScanType = ScanType.StaticAddress)]
-    public unsafe bool* _useOverrideSimulationTime = null;
-
     public Plugin()
     {
         Hooking.InitializeFromAttributes(this);
 
         unsafe
         {
-            *_useOverrideSimulationTime = true;
+            *BonePhysicsModule.GetForceOverrideSimulationTime() = true;
         }
     }
 
@@ -31,7 +24,7 @@ public sealed class Plugin : IDalamudPlugin
     {
         unsafe
         {
-            *_useOverrideSimulationTime = false;
+            *BonePhysicsModule.GetForceOverrideSimulationTime() = false;
         }
     }
 }
